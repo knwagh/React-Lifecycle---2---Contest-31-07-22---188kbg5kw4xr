3cd6b8ee89ba02d0a821399f42d2a44db86a6f2e
knwagh <107362567+knwagh@users.noreply.github.com>
2022-08-22 17:07:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,11 +12,7 @@ class App extends React.Component {
     super(props)
     this.state = {count:0}
     this.handleClick = this.handleClick.bind(this);
-
-  }
-  
-  compountDidMount(){
-    console.log("mounted");
+console.log("mounted");
   }
 
 
